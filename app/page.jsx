48bc6891fad632e83194dd9af68f64cea0920543
letiricetti2-alt <letiricetti2@gmail.com>
2025-11-27 19:52:17 -0300

    Create page.jsx

---
+++ app/page.jsx
@@ -0,0 +1,17 @@
+export default function Home() {
+  return (
+    <main
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "100vh",
+        fontFamily: "sans-serif",
+      }}
+    >
+      <h1>Gerador de Atividades Infantis</h1>
+      <p>O app está funcionando corretamente! 🚀</p>
+    </main>
+  );
+}
