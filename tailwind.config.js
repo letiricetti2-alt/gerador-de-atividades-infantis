6cee044427f805addce2eb81b1166f356636c1ee
letiricetti2-alt <letiricetti2@gmail.com>
2025-12-03 22:19:32 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,7 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}"
-  ],
-  theme: {
-    extend: {},
+theme: {
+  extend: {
+    fontFamily: {
+      baloo: ['"Baloo 2"', "sans-serif"],
+    },
   },
-  plugins: [],
-};
+},
