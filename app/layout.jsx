3abad642b17a98d7c67f3bac433ca74f500bd6ff
letiricetti2-alt <letiricetti2@gmail.com>
2025-12-03 22:31:40 -0300

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,7 +3,7 @@ import { Baloo_2 } from "next/font/google";
 
 const baloo = Baloo_2({
   subsets: ["latin"],
-  weight: ["400", "600", "700"],
+  weight: ["400", "600", "700", "800"],
   variable: "--font-baloo",
 });
 
