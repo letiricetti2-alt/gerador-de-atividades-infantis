5c7c263e8d6d307c82cb5e3acc012732e571aa4e
letiricetti2-alt <letiricetti2@gmail.com>
2025-12-05 14:00:33 -0300

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,9 +1,10 @@
+// app/layout.jsx
 import "./globals.css";
 import { Baloo_2 } from "next/font/google";
 
 const baloo = Baloo_2({
   subsets: ["latin"],
-  weight: ["400", "600", "700", "800"],
+  weight: ["400", "500", "600", "700", "800"],
   variable: "--font-baloo",
 });
 
@@ -16,7 +17,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="pt-BR">
       <body
-        className={`${baloo.variable} font-baloo bg-[#FDBA3A] text-[#1E1464] flex justify-center`}
+        className={`${baloo.variable} font-baloo bg-[#FFD83A] text-[#1E1464] flex justify-center`}
       >
         {children}
       </body>
