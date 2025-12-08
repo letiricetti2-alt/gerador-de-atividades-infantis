38544c90473077e1d3c02653aa719430536a8586
letiricetti2-alt <letiricetti2@gmail.com>
2025-12-08 20:40:36 -0300

    Update page.jsx

--- app/atividades-adaptadas/page.jsx
+++ app/atividades-adaptadas/page.jsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 export default function AtividadesAdaptadas() {
   return (
     <>
-      {/* Fundo azul igual às outras telas */}
+      {/* Fundo azul igual às outras páginas */}
       <style jsx global>{`
         html, body {
           background-color: #A9E4FF !important;
@@ -16,13 +16,13 @@ export default function AtividadesAdaptadas() {
       `}</style>
 
       <main className="flex justify-center items-center min-h-screen bg-[#A9E4FF]">
-        <div className="relative w-[480px] h-[950px]">
+        <div className="relative w-[480px] h-[950px] overflow-hidden">
           <Image
             src="/adaptada.jpeg"
             alt="Atividades Adaptadas"
             fill
             priority
-            className="object-contain"
+            className="object-cover"
           />
         </div>
       </main>
