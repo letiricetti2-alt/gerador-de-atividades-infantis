4720b3a1a42de08d9d4f2aed878563a3dcda5d6c
letiricetti2-alt <letiricetti2@gmail.com>
2025-12-08 20:57:52 -0300

    Update page.jsx

--- app/atividades-adaptadas/page.jsx
+++ app/atividades-adaptadas/page.jsx
@@ -5,25 +5,27 @@ import Image from "next/image";
 export default function AtividadesAdaptadas() {
   return (
     <>
+      {/* Fundo global idêntico */}
       <style jsx global>{`
         html, body {
           background-color: #A9E4FF !important;
-          height: 100%;
-          width: 100%;
           margin: 0;
           padding: 0;
+          height: 100%;
+          width: 100%;
           overflow: hidden;
         }
       `}</style>
 
+      {/* Container central com proporção igual à home */}
       <main className="flex justify-center items-center w-screen h-screen bg-[#A9E4FF]">
-        <div className="relative w-[100vw] h-[100vh] max-w-[530px] max-h-[990px] flex justify-center items-center">
+        <div className="relative flex justify-center items-center w-[480px] h-[950px]">
           <Image
             src="/adaptada.jpeg"
             alt="Atividades Adaptadas"
             fill
             priority
-            className="object-contain scale-[1.18]"
+            className="object-contain"
           />
         </div>
       </main>
