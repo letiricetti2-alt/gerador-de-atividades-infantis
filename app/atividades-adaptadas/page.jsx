f2b28c4d9c60f2964e81c929ea05123eef1ceabf
letiricetti2-alt <letiricetti2@gmail.com>
2025-12-08 20:17:16 -0300

    Update page.jsx

--- app/atividades-adaptadas/page.jsx
+++ app/atividades-adaptadas/page.jsx
@@ -1,29 +1,17 @@
 "use client";
 
 import Image from "next/image";
-import Link from "next/link";
 
 export default function AtividadesAdaptadas() {
   return (
-    <main className="page-wrapper bg-[#A9E4FF]">
-      <div className="relative w-[480px] max-w-full h-[950px] flex flex-col items-center justify-center">
-        <Image
-          src="/adaptada.jpeg"
-          alt="Atividades Adaptadas"
-          fill
-          priority
-          className="object-contain"
-        />
-
-        <div className="absolute bottom-20 w-full flex justify-center px-6">
-          <Link
-            href="/"
-            className="w-full bg-[#2ECC71] text-white font-semibold py-3 rounded-2xl shadow-md text-center text-lg"
-          >
-            Voltar para o início
-          </Link>
-        </div>
-      </div>
+    <main className="relative w-full min-h-screen flex items-center justify-center overflow-hidden bg-[#A9E4FF]">
+      <Image
+        src="/adaptada.jpeg"
+        alt="Atividades Adaptadas"
+        fill
+        priority
+        className="object-contain"
+      />
     </main>
   );
 }
