09a79c90bdba6cc235228a53a6fad42a3e101da5
letiricetti2-alt <letiricetti2@gmail.com>
2025-12-08 19:36:26 -0300

    Update page.jsx

--- app/atividades-adaptadas/page.jsx
+++ app/atividades-adaptadas/page.jsx
@@ -4,8 +4,8 @@ import Image from "next/image";
 
 export default function AtividadesAdaptadas() {
   return (
-    <main className="relative w-full min-h-screen flex items-center justify-center overflow-hidden bg-[#A9E4FF]">
-      <div className="relative w-full max-w-[480px] aspect-[3/4] flex items-center justify-center">
+    <main className="flex items-center justify-center min-h-screen bg-[#A9E4FF]">
+      <div className="relative w-full max-w-[390px] aspect-[9/16] flex items-center justify-center">
         <Image
           src="/adaptada.jpeg"
           alt="Atividades Adaptadas"
