fc96f90c418b68a303d66e6617fddb39a6d6a483
letiricetti2-alt <letiricetti2@gmail.com>
2025-12-09 17:29:33 -0300

    Create page.jsx

---
+++ app/perfil/page.jsx
@@ -0,0 +1,42 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Perfil() {
+  return (
+    <div
+      style={{
+        backgroundColor: "#b18be8", // fundo roxo suave
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        minHeight: "100vh",
+      }}
+    >
+      <div style={{ position: "relative", width: "480px", height: "950px" }}>
+        <Image
+          src="/perfil.jpeg"
+          alt="Tela do perfil"
+          fill
+          style={{ objectFit: "contain" }}
+          priority
+        />
+
+        {/* Área clicável - botão "Voltar ao Início" (exemplo) */}
+        <Link href="/">
+          <div
+            style={{
+              position: "absolute",
+              bottom: "40px",
+              left: "50%",
+              transform: "translateX(-50%)",
+              width: "60%",
+              height: "50px",
+              borderRadius: "20px",
+              cursor: "pointer",
+            }}
+          />
+        </Link>
+      </div>
+    </div>
+  );
+}
